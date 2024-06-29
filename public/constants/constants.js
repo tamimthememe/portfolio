c21c084858b0e044ab0d6da1330099316645d5d9
Tamim-The-Meme <127782275+Tamim-The-Meme@users.noreply.github.com>
2024-06-29 16:17:15 +0300

    Project Descriptions Feature

--- public/constants/constants.js
+++ public/constants/constants.js
@@ -134,7 +134,8 @@ export const work = [
     tech: ["HTML", "Tailwind CSS", "React", "GSAP", "Vercel"],
     mockup: tamimDevMockup,
     background: "../assets/images/gradients/gray-gradient.png",
-    url: "https://brainwave-seven-murex.vercel.app/",
+    longDesc:
+      "I developed tamim.dev(), my portfolio website, as a showcase of my projects and skills. It features dynamic animations using GSAP, engaging hover effects, and a filter feature for easy navigation through projects. Scroll animations enhance user interaction, while the design emphasizes aesthetics and responsiveness across all devices. Built with React and Next.js, tamim.dev() ensures efficient frontend development and seamless navigation. It offers straightforward data management for adding and updating content. Analytics integration through Vercel provides valuable insights into website performance and visitor behavior, enhancing overall user experience and optimization efforts.",
   },
   {
     category: "Live Site",
@@ -147,6 +148,8 @@ export const work = [
     mockup: brainwaveMockup,
     background: "../assets/images/gradients/purple-gradient.png",
     url: "https://brainwave-seven-murex.vercel.app/",
+    longDesc:
+      "I designed a landing page website for an AI tool named Brainwave, focusing on a captivating user experience. The site incorporates dynamic animations using GSAP, engaging hover effects, and immersive parallax animations to create a visually appealing interface. With a strong emphasis on aesthetic and responsive design principles, Brainwave ensures a seamless experience across various devices. It offers straightforward data management, making it easy to update content as needed. The frontend development was executed using React, combining modern technology with intuitive user interface design.",
   },
   {
     category: "Live Site",
@@ -159,6 +162,8 @@ export const work = [
     mockup: appleMockup,
     background: "../assets/images/gradients/gray-gradient.png",
     url: "https://apple-website-one-rose.vercel.app/",
+    longDesc:
+      "I created a website proposition for iPhone 15 and Apple, designed to showcase innovation and user engagement. The site features dynamic animations using GSAP, interactive hover effects, and immersive parallax animations to enhance user interaction. Utilizing Three.js, the website incorporates detailed 3D models, providing a realistic view of the iPhone 15 and Apple products. The design emphasizes aesthetics and responsiveness, ensuring a seamless experience across different devices. It offers easy data management for updating content effortlessly. Built with React, the frontend development integrates modern technology with intuitive user interface design. Additionally, the website includes analytics capabilities using Sentry, providing insights for continuous improvement.",
   },
   {
     category: "Live Site",
@@ -170,6 +175,9 @@ export const work = [
     tech: ["HTML", "CSS", "React", "Framer Motion", "Email.js"],
     mockup: securelyMockup,
     background: "../assets/images/gradients/gray-gradient.png",
+    url: "https://securely.pk/",
+    longDesc:
+      "I developed a website for an agency called Securely.pk, specializing in selling software solutions. Securely.pk features dynamic animations using Framer Motion and interactive hover effects, enhancing the overall user experience.\n\nThe website boasts an aesthetic and responsive design, ensuring it looks great on all devices. It offers easy data management, making it simple to add and remove information. The frontend development was done using React, and the site includes a direct email functionality using EmailJS, allowing clients to reach out easily.",
   },
   {
     category: "Design",
@@ -181,6 +189,9 @@ export const work = [
     tech: ["Figma"],
     mockup: securelyMockup,
     background: "../assets/images/gradients/gray-gradient.png",
+    longDesc:
+      "I designed the website for Securely Innovations Pvt. by initially categorizing important and less important data. Using Figma, I crafted the entire site starting from just a logo, ensuring a consistent color scheme throughout.\n\nThe design adheres to Schneiderman's principles and Nielsen's heuristics to ensure a superior user experience. The website features a hero section, about section, pricing section, testimonial section, feature section, and a contact page that allows emails to be sent directly to the client.",
+    colours: ["#FFFFFF", "#898989", "#D8D8D8", "#080808", "#146EF5"],
   },
   {
     category: "Website",
@@ -191,6 +202,8 @@ export const work = [
     tech: ["HTML", "CSS", "React", "Framer Motion", "API"],
     mockup: mimflixMockup,
     background: "../assets/images/gradients/red-gradient.png",
+    longDesc:
+      "I developed a website called Mimflix, meticulously designed to replicate the Netflix experience. Mimflix integrates real-time API data from OMDB, ensuring users have access to the latest movie information. The site features interactive hover animations that play video previews, adding an engaging layer to the browsing experience. The design is both impressive and intuitive, adhering to modern UX standards. Mimflix also customizes movie suggestions based on the user's location, offering a personalized experience. The entire frontend was crafted using React, ensuring a seamless, responsive, and dynamic user interface that enhances the overall user experience.",
   },
   {
     category: "Design",
@@ -203,6 +216,9 @@ export const work = [
     device: "mobile",
     mockup: wegoMockup,
     background: "../assets/images/gradients/green-gradient.png",
+    longDesc:
+      "I designed the concept for a software called WeGo, aimed at centralizing all modes of travel (flights, trains, buses) in Pakistan. The project utilizes Figma for prototyping, incorporates Nielsen's heuristics for usability, integrates maps for navigation, and features animations to enhance user interaction. WeGo focuses on aesthetic and UX design principles, ensuring a seamless and visually appealing experience. It includes wallet design for easy transactions and adheres to Schneiderman's principles for intuitive interface design. This comprehensive approach aims to streamline travel planning and booking processes for users across Pakistan.",
+    colours: ["#38D700", "#000000", "#979797", "#D9D9D9", "#FFFFFF"],
   },
   {
     category: "Website",
@@ -213,6 +229,8 @@ export const work = [
     tech: ["HTML", "CSS", "React", "Framer Motion"],
     mockup: nftsMockup,
     background: "../assets/images/gradients/purple-gradient.png",
+    longDesc:
+      "I developed a website called NFTsFeo, an NFT marketplace designed to provide a seamless user experience. NFTsFeo incorporates dynamic animations using Framer Motion and engaging hover effects, enhancing the site's interactivity. The website boasts an aesthetic and visually appealing design, making navigation enjoyable. Adding and removing data is straightforward, allowing for easy management of NFTs. The frontend was built using React, ensuring a responsive and efficient interface that caters to both creators and collectors.",
   },
   {
     category: "Design",
@@ -224,6 +242,9 @@ export const work = [
     tech: ["Figma"],
     mockup: razerMockup,
     background: "../assets/images/gradients/orange-gradient.png",
+    longDesc:
+      "Designed as a conceptual project, this landing page for the Razer and Lamborghini partnership showcases a seamless integration of advanced technologies and sophisticated design principles. Crafted using Figma for prototyping, the page adheres to Nielsen's heuristics to ensure intuitive usability. Dynamic animations captivate visitors, while maps integration provides geographical context, enhancing user engagement. The aesthetic design reflects the sleek, high-performance ethos of both brands, complemented by a wallet design feature for seamless transactions. Guided by Schneiderman's principles, the user experience (UX) prioritizes clarity and efficiency, offering a seamless journey through the world of gaming and automotive innovation. This conceptual landing page sets the stage for a digital experience that blends the thrill of gaming with the luxury of Lamborghini, inviting enthusiasts to explore and engage with the partnership's offerings.",
+    colours: ["#FF6B00", "#00FF00", "#FFFFFF", "#020202", "#000000"],
   },
   {
     category: "Design",
@@ -236,5 +257,8 @@ export const work = [
     device: "mobile",
     mockup: tamembersMockup,
     background: "../assets/images/gradients/purple-gradient.png",
+    longDesc:
+      "I designed the concept for a membership management software called Tamembers, focusing on usability and aesthetic appeal. The project includes Figma prototyping for visualizing the interface, Nielsen's heuristics for usability testing, and maps integration for location-based features. Animations are used to enhance user interaction, while the design emphasizes both aesthetic and UX principles. Wallet functionality is included for convenient transactions, and Schneiderman's principles guide intuitive interface design. Tamembers aims to provide a seamless experience for managing memberships effectively.",
+    colours: ["#1E1527", "#6B0083", "#8E6C6C", "#F4C2C2", "#D9D9D9"],
   },
 ];
--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -2,15 +2,19 @@
 
 import Image from "next/image";
 import { work } from "../../../public/constants/constants";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import closeIcon from "../../../public/assets/icons/Close.png";
+import linkIcon from "../../../public/assets/icons/link.png";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const Projects = () => {
   const mainRef = useRef();
   const projRef = useRef();
+
+  const [openModal, setOpenModal] = useState(null);
 
   useEffect(() => {
     gsap.to(mainRef.current, {
@@ -35,12 +39,12 @@ const Projects = () => {
     });
   }, []);
 
-  const handleClick = (url)=>{
-    window.location.href=url;
-  }
+  const handleClick = (url, item) => {
+    setOpenModal(item);
+  };
 
   return (
-    <section className="w-full h-full">
+    <section className="relative w-full h-full">
       <div className="pt-24 max-md:pt-10 max-md:px-7 md:w-[60%] mx-auto">
         <div className="">
           <div ref={mainRef} className="scale-75 origin-left">
@@ -61,7 +65,9 @@ const Projects = () => {
               <div
                 className="bg-[#171717] my-5 p-3 rounded-lg"
                 key={index}
-                onClick={()=> {item.url && handleClick(item.url)}}
+                onClick={() => {
+                  handleClick(item.url, item);
+                }}
               >
                 <div className="relative m-auto rounded-lg">
                   <div
@@ -75,7 +81,7 @@ const Projects = () => {
                           {item.date}
                         </p>
                       </div>
-                      <p className="text-[14px] text-gray-200 mb-2">
+                      <p className="max-md:hidden text-[14px] text-gray-200 mb-2">
                         {item.sub}
                       </p>
                       <Image
@@ -95,6 +101,80 @@ const Projects = () => {
           </div>
         </div>
       </div>
+      {openModal && (
+        <div>
+          <div
+            className="absolute left-0 top-0 w-[100%] h-[100%] bg-black-200 opacity-75 cursor-pointer"
+            onClick={() => setOpenModal("")}
+          />
+          <div
+            className="fixed bg-black-100 h-[80%] w-[60%] px-10 py-5 max-md:w-[80%] max-md:bottom-[3em] md:left-[15em] m-auto inset-x-0 inset-y-0 rounded-lg"
+          >
+            <div className="flex justify-between items-center">
+              <h1 className="md:page-header ">{openModal.name}</h1>
+              <Image
+                className="cursor-pointer"
+                src={closeIcon}
+                alt="Close Icon"
+                width={35}
+                onClick={() => setOpenModal("")}
+              />
+            </div>
+            <div className="overflow-y-auto h-[92%] mt-3 mb-2 no-scrollbar">
+              <div className="flex flex-wrap justify-center gap-4 h-full items-start ">
+                <div>
+                  <Image
+                    className="w-[24em] mt-3"
+                    src={openModal.mockup}
+                    alt={`${openModal.name} Mockup`}
+                  />
+                  {openModal.url && (
+                    <div className=" mt-2 ml-3 flex gap-2 items-center hover:opacity-50">
+                      <a
+                        target="_blank"
+                        className=" text-white"
+                        href={openModal.url}
+                      >
+                        Visit the Live Website Here
+                      </a>
+                      <Image src={linkIcon} alt="Link Icon" width={18} />
+                    </div>
+                  )}
+                </div>
+                <div className="w-[24em]">
+                  <h1 className="page-sub mt-5">The Concept</h1>
+                  <p className="page-desc">{openModal.longDesc}</p>
+                  <h1 className="page-sub mt-5">Tech Stack</h1>
+                  <div className="mt-1 mx-2 flex flex-wrap gap-2 mb-5">
+                    {openModal.tech.map((item, index) => (
+                      <div
+                        key={index}
+                        className="bg-black-300 px-2 py-1 rounded-md"
+                      >
+                        <p className="text-sm">{item}</p>
+                      </div>
+                    ))}
+                  </div>
+                  {openModal.colours && (
+                    <div className="">
+                      <h1 className="page-sub mb-2">Colours</h1>
+                      <div className="flex mb-5">
+                        {openModal.colours.map((colour, index) => (
+                          <div
+                            key={index}
+                            style={{ backgroundColor: colour }}
+                            className={`h-[10em] flex-1`}
+                          />
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
